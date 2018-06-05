cc4b664c58d35a47d3276643fbd20408103fff80
Jeffrey Teague <jefalteague@gmail.com>
2018-06-04 20:52:31 -0400

    the long haul to oop app begins

---
+++ oop_rewrites/db_singleton_oop.php
@@ -0,0 +1,41 @@
+<?php
+/*a singleton for a dbconnection: https://phpenthusiast.com/blog/the-singleton-design-pattern-in-php*/
+class ConnectDB {
+	
+	private static $instance;
+	private $connection;
+	private $dsn = 'mysql:host=localhost;dbname=my_guitar_shop2';
+	private $username = 'mgs_user';
+	private $password = 'pa55word';
+	private $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
+	
+	private function __construct() {
+
+		try {
+			$this->connection = new PDO($this->dsn, $this->username, $tis->password, $this->options);
+		} catch (PDOException $e) {
+			$error_message = $e->getMessage();
+			include();
+			exit();
+		}
+		
+	}
+	
+	public static getInstance() {
+		
+		if (empty(self::$instance)) {
+			self::$instance = new ConnectDB();
+		}
+		return self::$instance;
+		
+	}
+	
+	public function getConnection() {
+		
+		return $this->connection;
+		
+	}
+	
+}
+
+?>
